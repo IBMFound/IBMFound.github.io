20f13923105bc8963a036393947607ce421bf787
Justin Grierson <ju3tin@hotmail.co.uk>
2018-08-01 09:55:34 +0100

    pusj

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* global workbox:false */
 
-self.importScripts('docs/4.1/assets/js/vendor/{fileName}')
+self.importScripts('docs/4.1/assets/js/{fileName}')
 
 workbox.precaching.precacheAndRoute([])
